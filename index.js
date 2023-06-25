6c8eb5b8e91c297330728b98774a18a7a2a804bf
Aliaksei Kalupaila <alexey2928@gmail.com>
2023-06-25 18:11:27 -0400

    second commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname + "/build")));
+app.use(express.static(path.join(__dirname + "public")));
 
 const DISH_ATTRIBUTES = {
 	dish_name: " ",
